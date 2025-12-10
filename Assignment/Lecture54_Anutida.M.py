f765df5c5f8d817e030c3c59e492f00695779bd3
KedAnu <ked.anu17@gmail.com>
2025-12-10 18:29:45 +0700

    Create Lecture54_Anutida.M.py

---
+++ Assignment/Lecture54_Anutida.M.py
@@ -0,0 +1,40 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    print("Total price",price1+price2)
+    return vatCalculator(price1 + price2)
+
+if login()==True:
+    showMenu()
+    seleted=menuSelect()
+    if seleted==1:
+        price=int(input("Total price: "))
+        print("result = ",vatCalculator(price))
+    elif seleted==2:
+        print("total price + vat = ",priceCalculator())
+    else:
+        print("error")
+else:
+    print("try again")
