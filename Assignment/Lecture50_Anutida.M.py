3f2e568d3ee9883a3065cd17dede46b05a61cc81
KedAnu <ked.anu17@gmail.com>
2025-12-10 17:36:45 +0700

    Create Lecture50_Anutida.M.py

---
+++ Assignment/Lecture50_Anutida.M.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+
+x=int(input())
+y=int(input())
+addNumber(x,y)
+minusNumber(x,y)
+multiplyNumber(x,y)
+divideNumber(x,y)
